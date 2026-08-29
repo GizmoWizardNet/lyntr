db3ec9db03d614059784ce9324016aebd899f77b
GizmoWizardDev <c1b023harsh.pandey@gmail.com>
2026-08-29 11:16:52 +0530

    add our repo to the about page

--- src/routes/AboutContent.svelte
+++ src/routes/AboutContent.svelte
@@ -29,6 +29,7 @@
 	];
 
 	const links: Array<{ href: string; icon: any; label: string; external: boolean }> = [
+		{ href: 'https://github.com/GizmoWizardNet/lyntr', icon: Code, label: 'Our Github :D', external: true },
 		{ href: 'https://github.com/face-hh/lyntr', icon: Code, label: 'Original, archived repo', external: true },
 		{
 			href: 'https://github.com/NotHMRC/pylyntr',
