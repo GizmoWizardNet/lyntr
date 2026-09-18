c445f8aca9ff0da6d883dce1622c9fa2f50af825
GizmoWizardDev <c1b023harsh.pandey@gmail.com>
2026-09-18 22:53:34 +0530

    use AVIFs instead of GIFs

--- src/lib/lyntskins.ts
+++ src/lib/lyntskins.ts
@@ -6,16 +6,16 @@ export interface LyntskinDef {
 }
 
 export const LYNTSKIN_CATALOG: LyntskinDef[] = [
-	{ key: 'blueprint', name: 'Blueprint', price: 150, file: '/lyntskins/blueprint.gif' },
-	{ key: 'game_loop', name: 'Game Loop', price: 300, file: '/lyntskins/game_loop.gif' },
-	{ key: 'normal_city', name: 'Normal City', price: 500, file: '/lyntskins/normal_city.gif' },
-	{ key: 'peaceful_japan', name: 'Peaceful Japan', price: 1000, file: '/lyntskins/peaceful_japan.gif' },
-	{ key: 'silent_night', name: 'Silent Night', price: 1300, file: '/lyntskins/silent_night.gif' },
-	{ key: 'homebrew', name: 'Homebrew', price: 1500, file: '/lyntskins/homebrew.gif' },
-	{ key: 'mario', name: 'Mario', price: 2000, file: '/lyntskins/mario.gif' },
-	{ key: 'elite_city', name: 'Elite City', price: 5000, file: '/lyntskins/elite_city.gif' },
-	{ key: 'psx_fish', name: 'PSX Fish', price: 7500, file: '/lyntskins/fish.gif' },
-	{ key: 'eff_juan', name: 'Spinny Eff Juan Car', price: 4500, file: '/lyntskins/f1.gif'}
+	{ key: 'blueprint', name: 'Blueprint', price: 150, file: '/lyntskins/blueprint.avif' },
+	{ key: 'game_loop', name: 'Game Loop', price: 300, file: '/lyntskins/game_loop.avif' },
+	{ key: 'normal_city', name: 'Normal City', price: 500, file: '/lyntskins/normal_city.avif' },
+	{ key: 'peaceful_japan', name: 'Peaceful Japan', price: 1000, file: '/lyntskins/peaceful_japan.avif' },
+	{ key: 'silent_night', name: 'Silent Night', price: 1300, file: '/lyntskins/silent_night.avif' },
+	{ key: 'homebrew', name: 'Homebrew', price: 1500, file: '/lyntskins/homebrew.avif' },
+	{ key: 'mario', name: 'Mario', price: 2000, file: '/lyntskins/mario.avif' },
+	{ key: 'elite_city', name: 'Elite City', price: 5000, file: '/lyntskins/elite_city.avif' },
+	{ key: 'psx_fish', name: 'PSX Fish', price: 7500, file: '/lyntskins/fish.avif' },
+	{ key: 'eff_juan', name: 'Spinny Eff Juan Car', price: 4500, file: '/lyntskins/f1.avif'}
 ];
 
 export const LYNTSKIN_BY_KEY: Record<string, LyntskinDef> = Object.fromEntries(
